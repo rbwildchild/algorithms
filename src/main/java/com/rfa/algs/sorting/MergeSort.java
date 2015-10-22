49f7b48bc5c921ff89f1233a93e736860c15e848
Rafael Benitez <rafael_benitez@epam.com>
2015-10-22 11:46:55 -0500

    Merge sort

--- src/main/java/com/rfa/algs/sorting/MergeSort.java
+++ src/main/java/com/rfa/algs/sorting/MergeSort.java
@@ -1,5 +1,46 @@
 package com.rfa.algs.sorting;
 
-public class MergeSort {
+import java.util.Arrays;
+
+public class MergeSort<T> extends BaseSort<T> {
+
+	private Comparable<T>[] aux;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void sort(Comparable<T>[] a) {
+		aux = new Comparable[a.length];
+		System.out.println("Merge Sort " + Arrays.deepToString(a));
+		sort(a, 0, a.length - 1);
+	}
+
+	private void sort(Comparable<T>[] a, int lo, int hi) {
+		if (hi <= lo) {
+			System.out.println("Sort single: " + " lo " + lo + " hi " + hi);
+			return;
+		}
+		int mid = lo + (hi - lo) / 2;
+		System.out.println("Sort: " + " lo " + lo + " mid " + mid + " hi " + hi);
+		sort(a, lo, mid);
+		sort(a, mid + 1, hi);
+		merge(a, lo, mid, hi);
+	}
+
+	private void merge(Comparable<T>[] a, int lo, int mid, int hi) {
+		int i = lo, j = mid + 1;
+		System.out.println("Merge: " + " lo " + lo + " mid " + mid + " hi " + hi);
+		for (int k = lo; k <= hi; k++)
+			aux[k] = a[k];
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid)
+				a[k] = aux[j++];
+			else if (j > hi)
+				a[k] = aux[i++];
+			else if (less(aux[j], aux[i]))
+				a[k] = aux[j++];
+			else
+				a[k] = aux[i++];
+		}
+	}
 
 }
---
+++ src/test/java/com/rfa/algs/sorting/test/MergeSortTest.java
@@ -0,0 +1,9 @@
+package com.rfa.algs.sorting.test;
+
+import com.rfa.algs.sorting.MergeSort;
+
+public class MergeSortTest extends BaseSortTest {
+	public MergeSortTest() {
+		sortator = new MergeSort<>();
+	}
+}
